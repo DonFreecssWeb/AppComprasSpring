10a52cf179e0df3d8ee6ec4dcce4d21d1da70f5d
Jorge <george16180@live.com>
2023-02-01 13:54:03 -0500

    My first commit

---
+++ src/main/java/com/jorgemartinez/AppCompraVenta/AppCompraVentaApplication.java
@@ -0,0 +1,13 @@
+package com.jorgemartinez.AppCompraVenta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCompraVentaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCompraVentaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jorgemartinez/AppCompraVenta/AppCompraVentaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jorgemartinez.AppCompraVenta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCompraVentaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
